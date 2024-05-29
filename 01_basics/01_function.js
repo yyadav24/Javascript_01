ad075af8b88a1ad18c1bce6acecd538528abd73a
Yuvraj Yadav (IND) <yuvraj.yadav@protivitiglobal.in>
2024-05-29 17:27:00 +0530

    01-basics

---
+++ 01_basics/01_function.js
@@ -0,0 +1,10 @@
+const user ={
+    username: "Yuvraj",
+    age: 23,
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and the age is ${anyobject.age}`);
+}
+
+handleObject(user)
